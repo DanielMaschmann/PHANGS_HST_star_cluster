dbbf5c79d2ebe8fd2e95cc6ead3bd60252f3d748
Daniel Maschmann <daniel.maschmann@rwth-aachen.de>
2024-04-17 12:16:59 -0400

    added helper func for cutouts

--- helper_func.py
+++ helper_func.py
@@ -2,65 +2,47 @@ import numpy as np
 import dust_tools.extinction_tools
 import os
 from urllib3 import PoolManager
+from astropy.nddata import Cutout2D
+import astropy.units as u
+import warnings
 
 
-def download_file(file_path, url, unpack=False, reload=False):
-    """
-
+def get_img_cutout(img, wcs, coord, cutout_size):
+    """function to cut out a region of a larger image with an WCS.
     Parameters
     ----------
-    file_path : str or ``pathlib.Path``
-    url : str
-    unpack : bool
-        In case the downloaded file is zipped, this function can unpack it and remove the downloaded file,
-        leaving only the extracted file
-    reload : bool
-        If the file is corrupted, this removes the file and reloads it
+    img : ndarray
+        (Ny, Nx) image
+    wcs : astropy.wcs.WCS()
+        astropy world coordinate system object describing the parameter image
+    coord : astropy.coordinates.SkyCoord
+        astropy coordinate object to point to the selected area which to cutout
+    cutout_size : float or tuple
+        Units in arcsec. Cutout size of a box cutout. If float it will be used for both box length.
 
     Returns
     -------
-
+    cutout : astropy.nddata.Cutout2D object
+        cutout object of the initial image
     """
-    if reload:
-        # if reload file the file will be removed to re download it
-        os.remove(file_path)
-    # check if file already exists
-    if os.path.isfile(file_path):
-        print(file_path, 'already exists')
-        return True
+    if isinstance(cutout_size, tuple):
+        size = cutout_size * u.arcsec
+    elif isinstance(cutout_size, float) | isinstance(cutout_size, int):
+        size = (cutout_size, cutout_size) * u.arcsec
     else:
-        # download file
-        http = PoolManager()
-        r = http.request('GET', url, preload_content=False)
+        raise KeyError('cutout_size must be float or tuple')
 
-        if unpack:
-            with open(file_path.with_suffix(".gz"), 'wb') as out:
-                while True:
-                    data = r.read()
-                    if not data:
-                        break
-                    out.write(data)
-            r.release_conn()
-            # uncompress file
-            from gzip import GzipFile
-            # read compressed file
-            compressed_file = GzipFile(file_path.with_suffix(".gz"), 'rb')
-            s = compressed_file.read()
-            compressed_file.close()
-            # save compressed file
-            uncompressed_file = open(file_path, 'wb')
-            uncompressed_file.write(s)
-            uncompressed_file.close()
-            # delete compressed file
-            os.remove(file_path.with_suffix(".gz"))
-        else:
-            with open(file_path, 'wb') as out:
-                while True:
-                    data = r.read()
-                    if not data:
-                        break
-                    out.write(data)
-            r.release_conn()
+    # check if cutout is inside the image
+    pix_pos = wcs.world_to_pixel(coord)
+    if (pix_pos[0] > 0) & (pix_pos[0] < img.shape[1]) & (pix_pos[1] > 0) & (pix_pos[1] < img.shape[0]):
+        return Cutout2D(data=img, position=coord, size=size, wcs=wcs)
+    else:
+        warnings.warn("The selected cutout is outside the original dataset. The data and WCS will be None",
+                      DeprecationWarning)
+        cut_out = type('', (), {})()
+        cut_out.data = None
+        cut_out.wcs = None
+        return cut_out
 
 
 def plot_reddening_vect(ax,
